0220e2a3297def738a1fb0d07d3c7699976bbeea
luca007 <luca.clerot@gmail.com>
2025-03-21 17:25:23 -0300

    Desisti desse design

---
+++ js/config.js
@@ -0,0 +1,17 @@
+export default {
+    companyName: "ZZ Caça Vazamento e Desentupidora",
+    contactPhone: "(61) 99334-2979",
+    contactEmail: "zzcacavazamento@gmail.com",
+    whatsappLink: "https://wa.me/61993342979",
+    googleMapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3839.954!2d-48.110497!3d-15.794234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935bcb7dddd57517%200x532c2d44bf32f5d8!2sQNN%205%2C%20Ceil%C3%A2ndia%20-%20DF%2C%2072225-053!5e0!3m2!1spt-BR!2sbr!4v1700000000000",
+    address: "St. N QNN 5 - Ceilândia, Brasília - DF, 72225-053",
+    coordinates: {
+        latitude: -15.794234,
+        longitude: -48.110497
+    },
+    socialMedia: {
+        facebook: "https://www.facebook.com/zzcacavazamento",
+        instagram: "https://www.instagram.com/zz_cacavazamento/",
+        whatsapp: "https://wa.me/61993342979"
+    }
+}
